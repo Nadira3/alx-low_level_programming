9f1f0f45d2d0e3b8e0eb430f7aeb1a0ce7abfb35
Nadira3 <paitanun35@gmail.com>
2023-02-21 20:58:20 -0800

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 /**
  * main - prints first 98 fibonacci numbers
  * Return: 0
@@ -11,14 +12,26 @@ int main(void)
 	T[0] = 1;
 	T[1] = 2;
 
-	printf("%lu, %lu, ", T[0], T[1]);
+	printf("%lu, %.lu, ", T[0], T[1]);
 	for (i = 2; i < 98; i++)
 	{
-		T[i] = T[i - 2] + T[i - 1];
-		/* if (i < 97)
+		if (i < 90)
+		{
+			T[i] = T[i - 2] + T[i - 1];
 			printf("%lu, ", T[i]);
-		else */
-			printf("%lu", T[3]);
+		}
+		else
+		{
+			unsigned long x, y;
+
+			x = (T[i - 2] + T[i - 1]) / 1000000;
+			y = (T[i - 2] + T[i - 1]) % 1000000;
+
+			if (T[i] < 97)
+				T[i] = printf("%lu%lu, ", x, y);
+			else
+				T[i] = printf("%lu%lu", x, y);
+		}
 	}
 	printf("\n");
 	return (0);
