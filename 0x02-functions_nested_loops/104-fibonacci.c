dc920d6a0334b3315845afc80297bc27d2e2dae7
Nadira3 <paitanun35@gmail.com>
2023-02-21 22:26:01 -0800

    fibo correct

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,38 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 /**
- * main - prints first 98 fibonacci numbers
- * Return: 0
+ * main - prints 98 mumbers in the fibonacci series
  */
 int main(void)
 {
 	int i;
-	unsigned long T[98];
+	long double T[98];
 
-	T[0] = 1;
-	T[1] = 2;
+	T[0] = 1.00000000;
+	T[1] = 2.00000000;
 
-	printf("%lu, %.lu, ", T[0], T[1]);
+	printf("%.0Lf, %.0Lf, ", T[0], T[1]);
 	for (i = 2; i < 98; i++)
 	{
-		if (i < 90)
-		{
-			T[i] = T[i - 2] + T[i - 1];
-			printf("%lu, ", T[i]);
-		}
+		T[i] = T[i - 2] + T[i - 1];
+		if (i < 97)
+			printf("%.0Lf, ", T[i]);
 		else
-		{
-			unsigned long x, y;
-
-			x = (T[i - 2] + T[i - 1]) / 1000000;
-			y = (T[i - 2] + T[i - 1]) % 1000000;
-
-			if (T[i] < 97)
-				T[i] = printf("%lu%lu, ", x, y);
-			else
-				T[i] = printf("%lu%lu", x, y);
-		}
+			printf("%.0Lf", T[i]);
 	}
-	printf("\n");
 	return (0);
 }
