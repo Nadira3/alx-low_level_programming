a4cbd2453e723a1d97b54c14aa25b0689405eb30
Nadira3 <paitanun35@gmail.com>
2023-02-21 19:25:35 -0800

    tasks 9-11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @a: first number argument
+ * @b: second number argument
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - prints 9x table
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int k = i * j;
+
+			if (k < 10)
+			{
+				if (j > 0)
+					_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar(10);
+	}
+}
