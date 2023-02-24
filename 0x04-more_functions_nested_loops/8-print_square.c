582b8736e1f8fbbf38042f66eb0633e4ec997bb5
Nadira3 <paitanun35@gmail.com>
2023-02-23 21:25:34 -0800

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,19 +1,26 @@
-
-#include <stdio.h>
+#include "main.h"
+/**
+ * hash - puts #
+ * @n: number
+ */
 void hash(int n)
 {
 	int i;
 	for (i = 0; i < n; i++)
-		putchar('#');
+		_putchar('#');
 }
+/**
+ * print_square - prints a square
+ * @n: number
+ */
 void print_square(int n)
 {
 	int i;
 	if (n <= 0)
-		putchar('\n');
+		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
 		hash(n);
-		putchar('\n');
+		_putchar('\n');
 	}
 }
