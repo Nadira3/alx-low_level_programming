524d59acaef41eaef0d62dcd109ce3129a1820d8
Nadira3 <paitanun35@gmail.com>
2023-02-23 21:20:01 -0800

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * space - puts space
+ * @n: number
+ */
 void space(int n)
 {
 	int i;
 	for (i = 0; i < n; i++)
 		putchar(' ');
 }
+/**
+ * print_diagonal - prints a diagonal
+ * @n: number
+ */
 void print_diagonal(int n)
 {
 	int i;
