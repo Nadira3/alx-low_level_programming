ffff79ad90b2726adac56aadf77f512a5d445328
Nadira3 <paitanun35@gmail.com>
2023-02-24 21:49:39 -0800

    completed prime factor task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
+#include <math.h>
+/**
+ * main - finds the largest prime factor of a number
+ * Return: 0
+ */
+int isPrime(long n);
+
 int main(void)
 {
-	long i, n = 612852475143, prime;
-	for (i = 0; i < n; i++)
+	long x, i, n = 612852475143;
+
+	for (i = 1; i <= n; i++)
 	{
-		if ( n % i == 0)
+		if (n % i == 0 && isPrime(i))
 		{
-			if (i % 
-			prime = i;
+			n /= i;
+			x = i;
 		}
 	}
-	printf("%ld\n", prime);
-	return (0);
+	printf("%ld\n", x);
+}
+int isPrime(long n)
+{
+	long i, count = 0;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (count > 2)
+		{
+			break;
+		}
+		if (n % i == 0)
+		{
+			++count;
+		}
+	}
+	if (count == 2)
+		return (1);
+	else
+		return (0);
 }
