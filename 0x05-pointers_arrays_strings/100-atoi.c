9cab6c67e5894948b379266170d7fe06f9469155
Nadira3 <paitanun35@gmail.com>
2023-03-01 21:50:52 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,14 +4,19 @@
 int _atoi(char s[])  
 {  
 
-    int i, num = 0;  
+    int i, num = 0, minus = 0;  
 
     int n = strlen(s);
     
     for (i = 0; i < n; i++){
+	    if (s[0] == 45)
+		    minus++;
 	if (s[i] >= 48 && s[i] <= 58)
 	        num = num * 10 + (s[i] - 48);  
 	}
-    return (num);  
+    if (minus < 1)
+	    return (num);  
+    else
+	    return (-num);
 }  
 
