cd5de25a5d977f91d936f00f9170de9cf39a19ac
Nadira3 <paitanun35@gmail.com>
2023-03-02 07:21:39 -0800

    strncpy correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
-
-	for (j = 0; src[j]; j++)
-		;
+	int i;
 
 	for (i = 0; i < n && src[i]; i++)
-	{
 		dest[i] = src[i];
-	}
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
