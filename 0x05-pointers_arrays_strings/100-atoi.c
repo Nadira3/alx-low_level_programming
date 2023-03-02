ba3f80677bbb3a9bbcd33c7fb474704de3f51ba4
Nadira3 <paitanun35@gmail.com>
2023-03-01 21:06:00 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,18 +8,15 @@
 int _atoi(char *s)
 {
 	int i, j, k = 0;
-	int collate[80];
-	int INT[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	unsigned int num = 0;
 
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if ((s[i] - '0') == INT[j])
+			if ((s[i] - '0') >= 48 && (s[i] - '0' <= 58)
 			{
-				collate[k] = INT[j];
-				num = num * 10 + collate[k];
+				num = num * 10 + (s[i] - '0');
 				k++;
 			}
 			if (j > 8 && k > 0 && (!(s[i + 1] >= 48 && s[i + 1] <= 58)))
