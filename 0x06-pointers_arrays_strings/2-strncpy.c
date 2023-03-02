4559f02afee75bcaeecd550f2ae2ccc3c1f30008
Nadira3 <paitanun35@gmail.com>
2023-03-02 03:39:42 -0800

    strncpy correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,13 +3,14 @@
  * _strncpy - copies n bytes of a string to another
  * to destination string
  * @dest: destination
- * @source: source
+ * @src: source
  * @n: number of bytes
+ * Return: pointer to destination string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n && src[i]; i++)
 	{
 		dest[i] = src[i];
