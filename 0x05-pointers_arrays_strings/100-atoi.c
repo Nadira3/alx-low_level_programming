518041e2d2346225dd46e2126120df0df292d35e
Nadira3 <paitanun35@gmail.com>
2023-03-01 21:36:45 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,17 @@
-#include <stdio.h>
-#include <string.h>
-/**
- * _atoi - finds the length of a string
- * @s: pointer to string
- * Return: length of the string
- */
-int _atoi(char *s)
-{
-	int i, j, k = 0;
-	unsigned int num = 0;
+#include <stdio.h>  
+#include <string.h>  
 
-	for (i = 0; s[i]; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if ((s[i] - '0') >= 48 && (s[i] - '0' <= 58))
-			{
-				num = num * 10 + (s[i]);
-				k++;
-			}
-			if (j > 8 && k > 0 && (!(s[i + 1] >= 48 && s[i + 1] <= 58)))
-				return (num);
-		}
+int _atoi(char s[])  
+{  
+
+    int i, num = 0;  
+
+    int n = strlen(s);
+    
+    for (i = 0; i < n; i++){
+	if (s[i] >= 48 && s[i] <= 58)
+	        num = num * 10 + (s[i] - 48);  
 	}
-	if (k > 0)
-		return (num);
-	else
-		return (k);
+    return (num);  
+}  
 
-}
