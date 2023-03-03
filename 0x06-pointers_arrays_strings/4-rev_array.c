e149d9dbd0b808e0f49a7d639be56f3e81023247
Nadira3 <paitanun35@gmail.com>
2023-03-02 17:05:44 -0800

    rev-array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - compares two strings
+ * @a: array
+ * @n: length of array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j;
+	int temp[80];
+
+	for (i = 0, j = n - 1; i < n / 2; i++, j--)
+	{
+		temp[i] = a[i];
+		a[i] = a[j];
+		a[j] = temp[i];
+	}
+}
