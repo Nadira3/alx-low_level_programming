35bd84afe7dfca503e4d6a30d42fcdd043f430b7
Nadira3 <paitanun35@gmail.com>
2023-03-02 16:23:54 -0800

    test

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,12 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, j, diff = 0;
 
-	for (i = 0; s1[i] && s2[i]; i++)
+	for (i = 0, j = 0; s1[i] && s2[j]; i++, j++)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		{
+			diff = s1[i] - s2[j];
+			return diff;
+		}
 	}
-	return (s1[i] - s2[i]);
+	return (diff);
 }
