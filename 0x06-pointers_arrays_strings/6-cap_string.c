6c2bb84c98ccc8212933636f08b9a294764e072c
Nadira3 <paitanun35@gmail.com>
2023-03-02 18:40:58 -0800

    capstring

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,14 +7,26 @@
 char *cap_string(char *str)
 {
 	int i, j;
-	int separator[] = {9, 10, 32, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int separator[] = {9, 10, 32, 44, 59, 33, 63, 34, 40, 41, 123, 125};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < 13; j++)
+		if (str[i] == 46)
 		{
-			if (str[i] == separator[j])
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
 				str[i + 1] -= 32;
+				continue;
+			}
+			continue;
+		}
+		for (j = 0; j < 12; j++)
+		{
+			if (str[i] == separator[j] && str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] -= 32;
+				break;
+			}
 		}
 	}
 	return (str);
