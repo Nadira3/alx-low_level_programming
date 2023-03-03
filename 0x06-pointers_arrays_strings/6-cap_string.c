d4cc11228212d2c7e8936cdfb1f175b79648aa55
Nadira3 <paitanun35@gmail.com>
2023-03-02 18:59:37 -0800

    s0 correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,6 +20,8 @@ char *cap_string(char *str)
 			}
 			continue;
 		}
+		if (str[0] <= 122 && str[0] >= 97)
+			str[0] -= 32;
 		for (j = 0; j < 12; j++)
 		{
 			if (str[i] == separator[j] && str[i + 1] >= 97 && str[i + 1] <= 122)
