38b419c9d820bec933e142286c947347c33c0140
Nadira3 <paitanun35@gmail.com>
2023-03-09 21:29:37 -0800

    rev leinr

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,10 +6,10 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
+	{
 		_print_rev_recursion(s + 1);
-	s--;
-	if (*s != '\n')
 		_putchar(*s);
-	if (*s == '\n')
-		return;
+		s--;
+	}
+	return;
 }
