b122a40cbdc69fc396c9d9a28c0ec320aa5d0a20
Nadira3 <paitanun35@gmail.com>
2023-03-10 17:58:03 -0800

    tasks 0-3

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints the name of a program
+ * @argc: argument count
+ * @argv: argument variables
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
