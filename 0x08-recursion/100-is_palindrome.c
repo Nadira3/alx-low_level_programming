525016f4c054bca70d69f508b398f1a279aa37c2
Nadira3 <paitanun35@gmail.com>
2023-03-15 04:43:27 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,18 +2,27 @@
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: string
- * Return: 0 or 1
+ * Return: 0 || 1
  */
+int _strlen(char *s)
+{
+	if (!(*s))
+		return (0);
+	return (1 + _strlen(s + 1));
+}
+int checker(char *s, char *s2)
+{
+	if (*s != *s2)
+		return (0);
+	if (s - s2 == 1 || (s - s2 == 2 && *s == *s2))
+		return (1);
+	return (checker (s + 1, s2 - 1));
+}
 int is_palindrome(char *s)
 {
-	char *ptr = s;
-	
-	if (*s)
-		is_palindrome(s + 1);
+	char *s2 = s + (_strlen(s) - 1);
 	if (!(*s))
-		return (*(s - 1));
-	if (*s != *ptr)
-		return (0);
-	ptr++;
-	return (1);
+		return (1);
+	return (checker(s, s2));
+
 }
