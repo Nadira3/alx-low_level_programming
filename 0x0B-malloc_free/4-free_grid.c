84992464b61383c722ab1284a7173db3032e2125
Nadira3 <paitanun35@gmail.com>
2023-03-15 18:07:57 -0700

    freegrid final solution

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * free_grid - frees memory allocated to a 2D array
+ * @grid - pointer to 2D array
+ * @height: height of the array
+ */
 void free_grid(int **grid, int height)
 {
 	int i = 0;
