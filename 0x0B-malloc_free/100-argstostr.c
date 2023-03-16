d95c9c73e47bd3b08bcd4effb8f03d4752d7edef
Nadira3 <paitanun35@gmail.com>
2023-03-15 20:02:12 -0700

    argostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * argstostr - concatenates all the arguments of a program
- * @ac: argument count
- * @av: argument variable
- * Return: pointer to a list of argv printed on separate lines
+ * _strlen - finds the lenght of a string
+ * @s: string
+ * Return: lenght of string
  */
 int _strlen(char *s)
 {
@@ -11,30 +10,39 @@ int _strlen(char *s)
 		return (0);
 	return (1 + _strlen(s + 1));
 }
+/**
+ * argstostr - concatenates all the arguments of a program
+ * @ac: argument count
+ * @av: argument variable
+ * Return: pointer to a list of argv printed on separate lines
+ */
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int av_len[20];
 	int i, j, str_len = 0, tot_len;
+	int *av_len;
+
+	av_len = (int*)malloc(ac * sizeof(int));
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
-		av_len[i - 1] = _strlen(av[i]);
-		tot_len += av_len[i - 1];
+		av_len[i] = _strlen(av[i]);
+		tot_len += av_len[i];
 	}
-	str = malloc(sizeof(char) * (tot_len + ac));
+	str = malloc(sizeof(char) * (tot_len + ac + 1));
 	if (str == NULL)
 		return (NULL);
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; j < av_len[i - 1]; j++, str_len++)
+		for (j = 0; j < av_len[i]; j++, str_len++)
 		{
 			str[str_len] = av[i][j];
 		}
 		str[str_len] = '\n';
 		str_len++;
 	}
+	free(av_len);
 	return (str);
 }
