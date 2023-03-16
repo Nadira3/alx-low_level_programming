78b0ba63e262d395ac7edb8cfbfb985e15ac00ee
Nadira3 <paitanun35@gmail.com>
2023-03-15 17:29:54 -0700

    alloc final correction

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = malloc(height * height);
+	ptr = malloc(sizeof(int*) * height);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < width; i++)
