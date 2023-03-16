e6595d9967dcd841b7aee1be69aa058e5fd448cf
Nadira3 <paitanun35@gmail.com>
2023-03-16 10:51:22 -0700

    trial

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,11 +12,9 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = malloc(sizeof(int *) * height);
+	ptr = malloc(sizeof(int *) * height * width);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < height; i++)
-		ptr[i] = malloc(sizeof(int) * width);
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
