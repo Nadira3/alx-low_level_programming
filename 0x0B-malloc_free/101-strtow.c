1bd56c0eda9a782abf83f2a0f88be22f28102c3b
Nadira3 <paitanun35@gmail.com>
2023-03-17 13:14:40 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,14 +1,45 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * _strlen - finds the lenght of a string
  * @s: string
  * Return: string lenght.
  */
-int _strlen(char *s)
+int numWord(char *str)
 {
-	if (!(*s))
-		return (0);
-	return (1 + _strlen(s + 1));
+	int num = 0;
+
+	while (*str == ' ')
+		str++;
+	while (*str)
+	{
+		if (*str == ' ' && *(str + 1) != ' ')
+			num++;
+		str++;
+	}
+	return (num);
+}
+/**
+ * findWords - find words separated by a space in a string
+ * @str: string
+ * Return: pointer to strings
+ */
+int findWords(char *str)
+{
+	int str_len = 0; 
+
+	while (*str == ' ')
+		str++;
+	while (*str)
+	{
+		if (*str == ' ')
+		{
+			break;
+		}
+		str_len++;
+		str++;
+	}
+	return (str_len);
 }
 /**
  * strtow - splits a string
@@ -18,23 +49,43 @@ int _strlen(char *s)
 char **strtow(char *str)
 {
 	char **ptr;
-	int i;
+	int i, j = 0, wordnum = numWord(str);
 
-	if (str == NULL || str[0] == '\0')
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	ptr = malloc(sizeof(char) * _strlen(str));
+	ptr = malloc(sizeof(char *) * (wordnum + 1));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < _strlen(str); i++)
+	for (i = 0; i < wordnum; i++)
 	{
-		if (str[i] == ' ' && str[i + 1] != ' ')
-			**ptr = '\n';
-		else if (str[i] == ' ' && str[i + 1] == ' ')
+		ptr[i] = malloc(sizeof(char) * (findWords(str) + 1));
+		str = str + findWords(str);
+		if (ptr[i] == NULL)
 		{
+			for (j = 0; j < i; j++)
+			{
+				free(ptr[j]);
+			}
+			free(ptr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < wordnum; i++)
+	{
+		if (str[i] == ' ')
+		{
+			j++;
 			continue;
 		}
-		else
-			**ptr = str[i];
+		for (; j < findWords(str); j++)
+		{
+			ptr[i][j] = str[j];
+			str = str + findWords(str);
+		}
+		ptr[i][j] = '\n';
+		ptr[i][j + 1] = '\0';
+		j = j + 2;
 	}
+	ptr[i] = NULL;
 	return (ptr);
 }
