c3e49b2ea102e6fc1821ce81b6dde6cf6e5c510c
Nadira3 <paitanun35@gmail.com>
2023-03-18 10:30:58 -0700

    calloc fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr_size = nmemb * size;
 	if (arr_size > INT_MAX)
 		return (NULL);
-	ptr = malloc(sizeof(int) * nmemb);
+	ptr = malloc(sizeof(int) * arr_size);
 	if (ptr == NULL)
 		return (NULL);
 	int_ptr = (int *)ptr;
