9d7a545c064bb95290a2e744262741c6bc92776e
Nadira3 <paitanun35@gmail.com>
2023-03-17 19:20:32 -0700

    strnconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,7 @@
  * @n: number of bytes
  * Return: pointer to destination string
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+/*char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, k, m = 0;
 	char *ptr;
@@ -16,8 +16,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (j = 0; j < n && s2[j]; ++j)
 		;
-	if (s1 == NULL)
-		i = 0;
 	if (s2 == NULL)
 		j = 0;
 	ptr = malloc(i + j + 1);
@@ -35,4 +33,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	ptr[k] = '\0';
 	return (ptr);
+}*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len1, len2, total_len;
+	char *s3;
+
+	len1 = len2 = 0;
+	for (i = 0; s1 != NULL && s1[i]; i++)
+		len1++;
+	for (i = 0; s2 != NULL && s2[i]; i++)
+		len2++;
+	total_len = len1 + len2 + 1;
+	s3 = malloc(sizeof(char) * total_len);
+	if (s3 == NULL)
+		return (NULL);
+	for (i = 0; i < len1 && s1[i]; i++)
+		s3[i] = s1[i];
+	if (s1 == NULL)
+		i = 0;
+	for (j = 0; j < n && s2[j]; i++, j++)
+	{
+		s3[i] = s2[j];
+	}
+	return (s3);
 }
