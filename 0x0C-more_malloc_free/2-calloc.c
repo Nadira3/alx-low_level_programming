f8ed88f1e5bfdc090960a7cb5b7169c21b3c3124
Nadira3 <paitanun35@gmail.com>
2023-03-17 20:22:41 -0700

    calloc size adjustment

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr_size = nmemb * size;
 	if (arr_size > INT_MAX)
 		return (NULL);
-	ptr = (int *)malloc(sizeof(int) * arr_size);
+	ptr = malloc(sizeof(int) * arr_size);
 	if (ptr == NULL)
 		return (NULL);
 	int_ptr = (int *)ptr;
