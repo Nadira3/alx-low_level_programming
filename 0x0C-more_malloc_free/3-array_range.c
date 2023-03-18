f2431fffe23397e8d1965ed27995cf8e8fc21ac7
Nadira3 <paitanun35@gmail.com>
2023-03-17 21:23:09 -0700

    arrayrange

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <limits.h>
 /**
- * arrange_range - allocate nmemb bytes for array elements
+ * array_range - allocate nmemb bytes for array elements
  * @min: minimum value
  * @max: maximum value
  * Return: pointer to array
@@ -14,12 +14,10 @@ int *array_range(int min, int max)
 	if (min > max || min <= 0 || max <= 0)
 		return (NULL);
 	arr_size = (max - min) + 1;
-	if (arr_size > INT_MAX)
-		return (NULL);
 	int_ptr = malloc(sizeof(int) * arr_size);
 	if (int_ptr == NULL)
 		return (NULL);
-	for (i = 0; min < max; i++, min++)
+	for (i = 0; i < arr_size && min < max; i++, min++)
 		int_ptr[i] = min;
 	return (int_ptr);
 }
