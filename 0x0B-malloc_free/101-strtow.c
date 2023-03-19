d840775e1a34a91dea67ee92689c27fc9cf85433
Nadira3 <paitanun35@gmail.com>
2023-03-19 04:31:39 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strlen - finds the lenght of a string
- * @s: string
- * Return: string lenght.
+ * numWord - finds the number of words in a string
+ * @str: string
+ * Return: number of words
  */
 int numWord(char *str)
 {
@@ -20,26 +20,45 @@ int numWord(char *str)
 	return (num);
 }
 /**
- * findWords - find words separated by a space in a string
+ * findWords - find the number of characters in a word in a string
  * @str: string
- * Return: pointer to strings
+ * Return: number of characters
  */
 int findWords(char *str)
 {
-	int str_len = 0; 
+	int str_len = 0;
 
 	while (*str == ' ')
 		str++;
 	while (*str)
 	{
-		if (*str == ' ')
-		{
-			break;
-		}
 		str_len++;
+		if (*str == ' ')
+			break;
 		str++;
 	}
 	return (str_len);
+}
+/**
+ * rstrchr - changes pointer to string to new position of a word
+ * @s: string
+ * @c: character
+ * @wordnum: word position
+ * Return: pointer to character
+ */
+char *rstrchr(char *s, char c, int wordnum)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		if (*s == c && *(s + 1) != c)
+			count++;
+		if (count == wordnum)
+			return (s + 1);
+		s++;
+	}
+	return (NULL);
 }
 /**
  * strtow - splits a string
@@ -49,7 +68,8 @@ int findWords(char *str)
 char **strtow(char *str)
 {
 	char **ptr;
-	int i, j = 0, wordnum = numWord(str);
+	char *string = str;
+	int i, j, k, wordnum = numWord(str);
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
@@ -58,33 +78,20 @@ char **strtow(char *str)
 		return (NULL);
 	for (i = 0; i < wordnum; i++)
 	{
-		ptr[i] = malloc(sizeof(char) * (findWords(str) + 1));
-		str = str + findWords(str);
+		string = rstrchr(string, ' ', i + 1);
+		ptr[i] = malloc(sizeof(char) * findWords(string));
 		if (ptr[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
-			{
 				free(ptr[j]);
-			}
 			free(ptr);
 			return (NULL);
 		}
-	}
-	for (i = 0; i < wordnum; i++)
-	{
-		if (str[i] == ' ')
-		{
-			j++;
-			continue;
-		}
-		for (; j < findWords(str); j++)
-		{
-			ptr[i][j] = str[j];
-			str = str + findWords(str);
-		}
-		ptr[i][j] = '\n';
-		ptr[i][j + 1] = '\0';
-		j = j + 2;
+		k = findWords(string);
+		for (j = 0; j < k; j++, string++)
+			ptr[i][j] = *string;
+		ptr[i][j] = '\0';
+		string = str;
 	}
 	ptr[i] = NULL;
 	return (ptr);
