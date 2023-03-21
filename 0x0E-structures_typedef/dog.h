20c9fd602ff7940a727c8e6aeea50b37ed3f1da8
Nadira3 <paitanun35@gmail.com>
2023-03-20 20:37:00 -0700

    dog.h submission

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - has three elements
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
