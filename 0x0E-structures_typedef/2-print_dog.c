a1319ab55fa23d094d49bca3795f8012d99cee15
Nadira3 <paitanun35@gmail.com>
2023-03-20 21:46:46 -0700

    printdog submission

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <math.h>
+/**
+ * print_dog - prints members lf a struct named dog
+ * @d: name of struct
+ */
+void print_dog(struct dog *d)
+{
+	d->name == NULL ? printf("(nil)\n") : printf("Name: %s\n", d->name);
+	isnan(d->age) ? printf("(nil)\n") : printf("Age: %f\n", d->age);
+	d->owner == NULL ? printf("(nil)\n") : printf("Name: %s\n", d->owner);
+}
