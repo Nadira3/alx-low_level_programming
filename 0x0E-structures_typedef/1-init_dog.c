5ae6f29b22e83de75fc4867e403e6fb5cfdd2915
Nadira3 <paitanun35@gmail.com>
2023-03-21 20:24:23 -0700

    1-init-dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,11 +8,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *gee;
-
-	if (d != NULL)
-		gee = d;
-	gee->name = name;
-	gee->age = age;
-	gee->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
