4f6be795d613a03fdc87ce875544d94cd07f5427
Nadira3 <paitanun35@gmail.com>
2023-03-31 09:49:15 -0700

    printstring correction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,11 +8,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list params;
 	unsigned int i;
+	char *str;
 
 	va_start(params, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(params, char*));
+		str = va_arg(params, char*);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 		if (i < (n - 1) && separator)
 			printf("%s", separator);
 	}
