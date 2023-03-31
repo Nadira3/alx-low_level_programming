95b141238afda70c7a771006f486e40a17f9458a
Nadira3 <paitanun35@gmail.com>
2023-03-31 05:44:40 -0700

    printnumbers task submission

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints number arguments
+ * @separator: number separator
+ * @n: number of arguments
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list params;
+	unsigned int i;
+	const char *ptr = separator;
+
+	if (separator == NULL)
+		return;
+	va_start(params, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(params, int));
+		if (i < (n - 1))
+		{
+			while (*ptr)
+				printf("%c", *ptr++);
+			ptr = separator;
+		}
+	}
+	printf("\n");
+	va_end(params);
+}
