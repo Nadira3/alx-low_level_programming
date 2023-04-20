46a446a4b833270d8832acd9a02e3657e51bab67
Nadira3 <paitanun35@gmail.com>
2023-04-20 17:40:24 +0100

    mul correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,9 +1,25 @@
 #include "main.h"
 /**
- * main - multiplies two numbers
+ * _isdigit - checks if all the charcaters in an argument
+ * are digits
+ * @str: pointer to string
+ * Return: 1 || 0
+ */
+int _isdigit(char *str)
+{
+	while (*str)
+	{
+		if (!(*str >= 48 && *str <= 57))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+/**
+ * main - adds positive numbers
  * @argc: argument count
  * @argv: argument variable
- * Return: 0
+ * Return: 0 || 1
  */
 int main(int argc, char *argv[])
 {
@@ -12,10 +28,21 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);
+		exit(98);
 	}
-	for (i = 1; i < argc; i++)
-		mul *= atoi(argv[i]);
-	printf("%d\n", mul);
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (_isdigit(argv[i]))
+				mul *= atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+		printf("%d\n", mul);
+	}
 	return (0);
 }
