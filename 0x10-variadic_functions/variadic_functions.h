28fac37fc2cfdcb0ecc2430c03941389d700d872
Nadira3 <paitanun35@gmail.com>
2023-04-21 06:44:39 +0100

    variadic function update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,10 +4,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+/**
+ * struct op - has two elements
+ * @format: op format
+ * @f: function pointer
+ */
 typedef struct op
 {
-    char *format;
-    void (*f)(va_list pam);
+	char *format;
+	void (*f)(va_list pam);
 } form_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
