0cdd87d171375c1cf6064f607fbaf43addf14077
Nadira3 <paitanun35@gmail.com>
2023-04-28 05:36:53 +0100

    free_list commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a linked list
+ * @head: head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+
+}
