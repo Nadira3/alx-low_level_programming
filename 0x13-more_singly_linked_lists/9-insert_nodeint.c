1c133f44bd8ffd77a649d27923898457f6b75309
Nadira3 <paitanun35@gmail.com>
2023-05-02 05:48:58 -0700

    handled the error: if it is not possible to add the new node at index idx, do not add the new node and return NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * listint_len - finds the number of elements in a list
+ * @h: pointer to list
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h)
+	{
+		while (h)
+		{
+			count++;
+			h = h->next;
+		}
+	}
+	return (count);
+}
+/**
  * insert_nodeint_at_index - inserts a node at the given index
  * @head: pointer to list pointer
  * @idx: index
@@ -16,6 +35,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (*head)
 		{
+			if (idx > listint_len(*head))
+				return (NULL);
 			newNode = malloc(sizeof(listint_t));
 			if (!newNode)
 				return (newNode);
