2a851b595e85e5c5a6142d557d84c60e0d68d565
Nadira3 <paitanun35@gmail.com>
2023-05-02 05:16:32 -0700

    get_nodeint correction

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * listint_len - finds the number of elements in a list
+ * @h: pointer to list
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h)
+	{
+		while (h)
+		{
+			count++;
+			h = h->next;
+		}
+	}
+	return (count);
+}
+/**
  * get_nodeint_at_index - gets node at given index
  * @head: list pointer
  * @index: node position
@@ -12,6 +31,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head)
 	{
+		if (index > listint_len(head))
+			return (NULL);
 		temp = head;
 		while (i++ < index)
 			temp = temp->next;
