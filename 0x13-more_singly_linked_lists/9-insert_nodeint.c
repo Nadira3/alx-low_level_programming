2555611852ee8295e9cc6a52146fec78c3b2ed51
nadira <nadira@ubuntu20.myguest.virtualbox.org>
2023-05-02 20:58:30 +0100

    handled testcase: idx < 0 ? return NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -81,7 +81,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (*head)
 	{
-		if (idx > listint_len(*head))
+		if (idx > listint_len(*head) || idx < 0)
 			return (NULL);
 		newNode = malloc(sizeof(listint_t));
 		if (!newNode)
