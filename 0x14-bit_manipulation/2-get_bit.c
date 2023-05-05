f91f3cc71649217b4cdfdd384d9ba55dc67712b8
Nadira3 <paitanun35@gmail.com>
2023-05-05 04:14:01 -0700

    handled error: return error for exceeding index number

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,14 +10,14 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int i = 0;
 
 	n = (long int)n < 0 ? -n : n;
-	if (index < 0)
+	if ((int)index < 0 || index > (sizeof(n) * 8))
 		return (-1);
-	while (n > 0 && index < (sizeof(n) * 8))
+	while (n > 0)
 	{
-		n /= 2;
-		i++;
 		if (i == index)
 			return (n % 2);
+		n /= 2;
+		i++;
 	}
-	return (-1);
+	return (0);
 }
