8f94ce7880e40a4bdbfb2372bc19b43202bc50fe
Nadira3 <paitanun35@gmail.com>
2023-05-04 19:12:54 -0700

    get_bit correction

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int i = 0;
 
+	n = (long int)n < 0 ? -n : n;
 	if ((int)index < 0)
 		return (-1);
-	n = (long int)n < 0 ? -n : n;
 	while (n > 0)
 	{
 		n /= 2;
