d27829f5151ac7bc4b370c7ab11a34ebb151d486
Nadira3 <paitanun35@gmail.com>
2023-05-10 17:23:10 +0000

    create_file first submission

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strlen - finds the length of a string
+ * @text: string argument
+ * Return: length of string
+ */
+int _strlen(char *text)
+{
+	if (!*text)
+		return (0);
+	return (1 + _strlen(text + 1));
+}
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to create
+ * @text_content: null terminated string to be written to the file
+ * Return: 1 on success, -1 on failure (file can not be created, file
+ * can not be written, write “fails”, etc…)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int n, len = _strlen(text_content);
+
+	n = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (n == -1)
+		return (n);
+	write(n, text_content, len);
+	close(n);
+	return (1);
+}
