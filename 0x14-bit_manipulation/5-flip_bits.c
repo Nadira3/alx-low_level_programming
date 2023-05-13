b80c11bc8ae9600292574f6f6da327c18ba3a10d
Nadira3 <paitanun35@gmail.com>
2023-05-13 19:19:58 +0000

    flip_bits correction

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,8 +8,31 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int flip_num = (n ^ m) && m;
+	unsigned long int bit_len = sizeof(n) * 8;
+	/* finds the no of bits of n by multiplying its size by 8 bytes */
+	int i, flag = 0;
+	/**
+	 * declare a flag to detect when the most significant bit in a binary
+	 * is found, to prompt printing to standard output with necessary
+	 * condition
+	 */
+	unsigned long int bit_n, bit_m;
 
-	return (flip_num);
+	for (i = bit_len - 1; i >= 0; i--)
+	{
+		bit_n = (n >> i) & 1;
+		bit_m = (m >> i) & 1;
+		/**
+		 * within the loop, shift the binary forn of the integer
+		 * i times to get the value of the right most bit and compare
+		 * it with a constant value of 1 using the & bitwise operator
+		 * and assign the value to bit
+		 */
+		if (bit_n != bit_m)
+		{
+			bit_m = bit_n;
+			flag++;	
+		}
+	}
+	return (flag);
 }
-
