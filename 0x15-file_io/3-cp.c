16c9555bd71aa62c005d4b026535e1f9a2eec3ee
Nadira3 <paitanun35@gmail.com>
2023-05-13 18:12:53 +0000

    handled error: write error msg to stderr before exit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,10 +11,7 @@ int main(int ac, const char **av)
 	char *ptr;
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	n_one = open(av[1], O_RDONLY);
 	if (n_one == -1)
 	{
@@ -26,18 +23,18 @@ int main(int ac, const char **av)
 		return (-1);
 	n_two = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (n_two == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	while ((n_byte = read(n_one, ptr, BUFSIZ)) > 0)
 	{
 		n_write = write(n_two, ptr, n_byte);
 		if (n_write == -1)
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	if (n_byte == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
+	}
 	ca = close(n_one);
 	cb = close(n_two);
 	if (ca == -1 || cb == -1)
