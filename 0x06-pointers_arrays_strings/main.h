b10077c93109dbf94830781a2c1b6ba92f47f49f
Nadira3 <paitanun35@gmail.com>
2023-06-10 12:34:15 +0000

    fixed error: implicit declaration of function ‘strlen’

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 int _putchar(int);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
