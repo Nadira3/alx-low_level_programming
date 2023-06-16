af2775c7b495e91b70d68d597ff8c3f0c0d2f42e
Nadira3 <paitanun35@gmail.com>
2023-06-16 05:14:02 +0000

    tasks 0 and 1 corrwctions

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,7 +7,7 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t len = 1;
+	size_t len = 0;
 	const dlistint_t *ptr;
 
 	if (h)
@@ -21,5 +21,5 @@ size_t print_dlistint(const dlistint_t *h)
 		}
 		printf("%d\n", ptr->n);
 	}
-	return (len);
+	return (len + 1);
 }
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,7 +7,7 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t len = 1;
+	size_t len = 0;
 	const dlistint_t *ptr;
 
 	if (h)
@@ -19,5 +19,5 @@ size_t dlistint_len(const dlistint_t *h)
 			len++;
 		}
 	}
-	return (len);
+	return (len + 1);
 }
