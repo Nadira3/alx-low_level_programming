3bcffc751981cec22efb5d6fc222e207d141ef09
Nadira3 <paitanun35@gmail.com>
2023-07-17 05:39:55 +0100

    added check that hash table is not null

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,18 +1,26 @@
 #include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value from hash table
+ * @ht: hash table
+ * @key: used to retrive index of hash table array
+ * Return: value at key index
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *ptr = NULL;
 	unsigned long int idx;
 
-	if (key && *key)
-		idx = key_index((const unsigned char *)key, ht->size);
-	ptr = ht->array[idx];
-
-	while (ptr)
+	if (ht && key && *key)
 	{
-		if (!strcmp(ptr->key, key))
-			return (ptr->value);
-		ptr = ptr->next;
+		idx = key_index((const unsigned char *)key, ht->size);
+		ptr = ht->array[idx];
+
+		while (ptr)
+		{
+			if (!strcmp(ptr->key, key))
+				return (ptr->value);
+			ptr = ptr->next;
+		}
 	}
-	return (0);
+	return (NULL);
 }
