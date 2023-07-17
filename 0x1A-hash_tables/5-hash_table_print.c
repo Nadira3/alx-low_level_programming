46b81c2e3303ce59d2060e8f063e2573109ab902
Nadira3 <paitanun35@gmail.com>
2023-07-17 06:11:10 +0100

    hash table print with decorations and separators included

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,20 +1,53 @@
 #include "hash_tables.h"
 
+/**
+ * is_empty - checks if a hash table is empty
+ * @ht: hash table
+ * @idx: current position/index of array in table
+ * Return: 1 if table is empty || 0 otherwise
+ */
+int is_empty(const hash_table_t *ht, unsigned int idx)
+{
+	unsigned int i;
+
+	if (ht)
+	{
+		for (i = idx; i < ht->size; i++)
+		{
+			if (ht->array[i])
+				return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int i;
 	hash_node_t *ptr = NULL;
 
-	for (i = 0; i < ht->size; i++)
+	if (ht)
 	{
-		if (ht->array[i])
+		printf("{");
+		for (i = 0; i < ht->size; i++)
 		{
-			ptr = ht->array[i];
-			while (ptr)
+			if (ht->array[i])
 			{
-				printf("%s: %s", ptr->key, ptr->value);
-				ptr = ptr->next;
+				ptr = ht->array[i];
+				while (ptr)
+				{
+					printf("\'%s\': \'%s\'", ptr->key, ptr->value);
+					ptr = ptr->next;
+				}
+				if (!ptr && !is_empty(ht, i + 1))
+					printf(", ");
 			}
 		}
+		printf("}");
+		printf("\n");
 	}
 }
